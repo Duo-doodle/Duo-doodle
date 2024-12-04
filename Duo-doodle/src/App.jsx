f31f8d169c60f6c1c41e568ecec4d34c44c5ed49
PegahMazhin <nirvana.pva@gmail.com>
2024-12-04 13:12:49 -0600

    Adding App.jsx

--- Duo-doodle/src/App.jsx
+++ Duo-doodle/src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import React, { useState } from "react";
+import Login from "./components/Login";
 
 function App() {
-
+  const [user, setUser] = useState(null);
 
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      {!user ? (
+        <Login onLogin={(username) => setUser(username)} />
+      ) : (
+        <div>Welcome, {user}!</div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
