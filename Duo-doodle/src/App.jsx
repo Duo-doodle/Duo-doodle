ee638923123bbf77ab5b35cf7fc71283ad88b3c4
EngineerMoore <alexiashalise@gmail.com>
2024-12-04 14:59:22 -0600

    combined components folders and fixed how-to import

--- Duo-doodle/src/App.jsx
+++ Duo-doodle/src/App.jsx
@@ -1,15 +1,23 @@
-import HowTo from '../components/HowTo'
-import './App.css'
+
+import HowTo from './src/components/HowTo.js';
+import React, { useState } from "react";
+import Login from "./components/Login";
 
 function App() {
-
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <HowTo />
-    
-    </>
-  )
+      <div>
+        {!user ? (
+          <Login onLogin={(username) => setUser(username)} />
+        ) : (
+          <div>Welcome, {user}!</div>
+        )}
+      </div>
+   </>
+  );
 }
 
-export default App
+export default App;
