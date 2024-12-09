9e2f3a1124918841fbf449d57fe24ce754ebba7f
EngineerMoore <alexiashalise@gmail.com>
2024-12-09 13:46:20 -0600

    added timer to drawing submission page

---
+++ Duo-doodle/src/components/DrawingSubmission.jsx
@@ -0,0 +1,51 @@
+import { useState, useRef, useEffect } from "react";
+
+const DrawingSubmission = () => {
+  const Ref = useRef(null)
+  const [ timer, setTimer ] = useState(`1:00`);
+
+  useEffect(() => {
+    let timeRemaining = 60;
+
+    const timeDecrement = () => {
+      if (timeRemaining <= 0) {
+        setTimer(`0:00`);
+        clearInterval(startTimer);
+        return;
+      }
+
+      if (timeRemaining <= 30){
+        const addAlert = () => {
+          const timerElement = document.querySelector(`.timer`);
+          timerElement.id = "timer-alert";
+          Ref.current = timerElement.id;
+        }
+        if (!Ref.current) addAlert();
+      }
+    
+      timeRemaining--;
+      
+      const minutes = Math.floor(timeRemaining/60);
+      const seconds = timeRemaining % 60;
+      
+      const clock = `${minutes}:${seconds > 9 ? seconds: `0${seconds}`}`;
+      setTimer(clock); 
+    };
+    
+    const startTimer = setInterval(timeDecrement, 1000);
+
+    return () => clearInterval(startTimer);
+  
+  }, [])
+ 
+
+  return (
+    <>
+      <h1>Your Turn to draw!</h1>
+      <h2>Topic: MEADOW</h2>
+      <p className ="timer">{timer}</p>
+    </>
+  );
+}
+
+export default DrawingSubmission
